a5d648ebbd2c9d35aa23a1e35d8c54988dbf648f
Jay Trivedi <61327228+JAY-TRIVEDI-007@users.noreply.github.com>
2024-08-01 01:34:51 +0530

    Tailwindcss, routes, services, guard and login, signup and dashboard component added.

--- angular-dashboard/src/app/app.routes.ts
+++ angular-dashboard/src/app/app.routes.ts
@@ -1,3 +1,22 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {PageNotFoundComponent} from './shared/page-not-found/page-not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    loadComponent: () => import('./login/login.component').then(c => c.LoginComponent),
+    data: {title: 'Login to app'}
+  },
+  {
+    path: 'signup',
+    loadComponent: () => import('./signup/signup.component').then(c => c.SignupComponent),
+    data: {title: 'Signup to app'}
+  },
+  {
+    path: '',
+    loadComponent: () => import('./dashboard/dashboard.component').then(c => c.DashboardComponent),
+    pathMatch: 'full',
+    data: {title: 'Welcome to dashboard!'}
+  },
+  {path: '**', component: PageNotFoundComponent, pathMatch: 'full', data: {title: 'Page not found'}},
+];
